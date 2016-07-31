dd285af7e3e91f3b2e01c924d23c2b83b66cad6a
czumczum <e.mindjunk@gmail.com>
2016-07-31 21:42:55 +0200

    week16: section 1, RWD added

--- Weekly webdev challenge/week 16/js/app.js
+++ Weekly webdev challenge/week 16/js/app.js
@@ -1,10 +1,22 @@
 $(document).ready(function () {
 
-    //Header - menu handling
-    var menu = $('#menu');
-    menu.on("click", function () {
-        menu.children().toggle('active');
+    //Header - menu handling for mobile
+    $(window).resize(function(){
+        if ($(window).width() <= 520) {
+            var menu = $('#menu');
+            menu.on("click", function () {
+                menu.children().toggle('active');
+            });
+        }
+        else {
+            var menu = $('#menu');
+            menu.off();
+        }
     });
 
-
+    //Nav - menu navigation
+    var menuLis = $('#menu li');
+    menuLis.each(function () {
+        console.log('test'); //TODO function for navigate
+    })
 });
